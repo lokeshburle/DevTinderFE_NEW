5ab459e4badc9ae25d9a0d691e0e474a7de35333
devikanth28 <devikanth28@gmail.com>
2025-02-20 22:41:37 +0530

    set up redux store and login functionality done

--- README.md
+++ README.md
@@ -8,3 +8,12 @@
 - create a Navbar seperate component file
 - install react-router-dom
 - create browserRouterDom and setup the route things
+- create login page
+- install the axios
+- cors - install cors in backend => add the configuration: origin, credentials:true
+- whenever ur making API call so pass the axios => creditials:true
+- Install redux toolkit
+- install react-redux + @reduxjs/toolkit => create a configureStore => add Provider to the application =>     createSlice => add reducer to the store
+- login and see if your data is coming properly in the store
+- Navbar should be update as soon as user log in
+- Refactor our code to add constants file + create a components folder
--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,31 @@
 import { useState } from 'react'
-import Navbar from './Navbar'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Body from './Body'
-import Login from './Login'
-import Profile from './Profile'
+import Body from './components/Body'
+import Login from './components/Login'
+import Profile from './components/Profile'
+import { Provider } from 'react-redux'
+import appStore from './utils/appStore'
+import Feed from './components/Feed'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
+    <Provider store={appStore}>
     <BrowserRouter basename='/'>
     <Routes>
     
       <Route path="/" element={<Body/>}>
         <Route path="/login" element={<Login/>}/>
         <Route path="/profile" element={<Profile/>}/>
+        <Route path="/feed" element={<Feed/>}/>
       </Route>
     </Routes>
     
     </BrowserRouter>
+    </Provider>
    
      
       
